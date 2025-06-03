cbfdf20d24f30603c4a2b2ce4fd43b008fdc5782
Mr Lii <leetamparega@gmail.com>
2025-06-03 06:02:40 +0000

    docs: create Developer entity and add the required fields

--- src/main/java/lii/buildmanstertaskmanagerapi/entity/jpa/Developer.java
+++ src/main/java/lii/buildmanstertaskmanagerapi/entity/jpa/Developer.java
@@ -1,4 +1,38 @@
 package lii.buildmanstertaskmanagerapi.entity.jpa;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.autoconfigure.info.ProjectInfoProperties;
+
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Developer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    @ElementCollection
+    private Set<String> roles;
+
+    @OneToMany(mappedBy = "developer")
+    private List<Task> tasks;
 }
