8d0581c8cd2a21e49f457500fc9f80fc4bb58317
Mr Lii <leetamparega@gmail.com>
2025-06-03 16:58:53 +0000

    docs: add constructors for TaskNotFoundExceptions

--- src/main/java/lii/buildmansterprojectmanagerapi/exception/TaskNotFoundException.java
+++ src/main/java/lii/buildmansterprojectmanagerapi/exception/TaskNotFoundException.java
@@ -1,11 +1,11 @@
 package lii.buildmansterprojectmanagerapi.exception;
 
-public class ProjectNotFoundException extends RuntimeException {
-    public ProjectNotFoundException(Long id) {
-        super("Project with id: " + id + " not found");
+public class TaskNotFoundException extends RuntimeException {
+    public TaskNotFoundException(Long id) {
+        super("Task with id: " + id + " not found");
     }
 
-    public ProjectNotFoundException(String message) {
+    public TaskNotFoundException(String message) {
         super(message);
     }
 
