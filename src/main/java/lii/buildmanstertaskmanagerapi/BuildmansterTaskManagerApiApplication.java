d7e20638c975680e3fae5c8bd4aca3e4ba0f00d0
Mr Lii <leetamparega@gmail.com>
2025-06-03 04:29:57 +0000

    config: Set up project structure

---
+++ src/main/java/lii/buildmanstertaskmanagerapi/BuildmansterTaskManagerApiApplication.java
@@ -0,0 +1,13 @@
+package lii.buildmanstertaskmanagerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildmansterTaskManagerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuildmansterTaskManagerApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lii/buildmanstertaskmanagerapi/BuildmansterTaskManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package lii.buildmanstertaskmanagerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildmansterTaskManagerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
