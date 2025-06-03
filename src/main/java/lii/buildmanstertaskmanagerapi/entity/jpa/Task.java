88731f5458a0c65d0b3e1e6e458de32b373a4a25
Mr Lii <leetamparega@gmail.com>
2025-06-03 06:02:20 +0000

    docs: create Task entity and add the required fields

--- src/main/java/lii/buildmanstertaskmanagerapi/entity/jpa/Task.java
+++ src/main/java/lii/buildmanstertaskmanagerapi/entity/jpa/Task.java
@@ -1,4 +1,42 @@
 package lii.buildmanstertaskmanagerapi.entity.jpa;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lii.buildmanstertaskmanagerapi.enums.TaskStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.usertype.BaseUserTypeSupport;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private TaskStatus status;
+
+    private LocalDate dueDate;
+
+    @ManyToOne
+    @JoinColumn(name = "project_id", nullable = false)
+    private Project project;
+
+    @ManyToOne
+    @JoinColumn(name = "developer_id")
+    private Developer developer;
 }
