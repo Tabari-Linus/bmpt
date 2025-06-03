bd88f0a5777fcc7ce0b66c6b0e2fcdcd5c29ab73
Mr Lii <leetamparega@gmail.com>
2025-06-03 06:01:56 +0000

    docs: create Project entity and add the required fields

--- src/main/java/lii/buildmanstertaskmanagerapi/entity/jpa/Project.java
+++ src/main/java/lii/buildmanstertaskmanagerapi/entity/jpa/Project.java
@@ -1,4 +1,39 @@
 package lii.buildmanstertaskmanagerapi.entity.jpa;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lii.buildmanstertaskmanagerapi.enums.ProjectStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    @Column
+    private String description;
+
+    private LocalDateTime deadline;
+
+    @Enumerated(EnumType.STRING)
+    private ProjectStatus projectStatus;
+
+    @OneToMany(mappedBy = "project", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Task> tasks;
+
 }
