72951b56a3da50311aff1563e1ff938c7da61150
Mr Lii <leetamparega@gmail.com>
2025-06-04 11:33:01 +0000

    docs: annodate Main appliction class with EnableJpaRepositories

--- src/main/java/lii/buildmansterprojectmanagerapi/BuildmansterTaskManagerApiApplication.java
+++ src/main/java/lii/buildmansterprojectmanagerapi/BuildmansterTaskManagerApiApplication.java
@@ -2,8 +2,10 @@ package lii.buildmansterprojectmanagerapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class BuildmansterTaskManagerApiApplication {
 
     public static void main(String[] args) {
